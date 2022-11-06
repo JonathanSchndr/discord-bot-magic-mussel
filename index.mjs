b6d8a7c439c45e8967b66d498c12c13391e5a7a1
Alexander Gaal <alexander.gaal@live.at>
2022-11-07 00:19:20 +0100

    update dependencies

---
+++ index.mjs
@@ -0,0 +1,29 @@
+import * as dotenv from "dotenv";
+import discord from "discord.js";
+
+dotenv.config();
+
+const token = process.env.DISCORD_BOT_TOKEN;
+const {Client, GatewayIntentBits} = discord;
+
+const intents = [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent
+];
+
+const client = new Client({intents});
+client.login(token);
+
+client.on('messageCreate', (message) => {
+  if (message.content.startsWith('MM:') && message.content.endsWith('?')) {
+    fetch('https://yesno.wtf/api')
+      .then((response) => response.json())
+      .then((response) => {
+        message.reply({
+          content: response.answer,
+          files: [response.image]
+        });
+      });
+  }
+});
