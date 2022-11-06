428ce85d9e53ffbc7bf65a5540f26364e0fcc532
Alexander Gaal <alexander.gaal@live.at>
2022-11-07 00:22:59 +0100

    update dependencies

--- index.mjs
+++ index.mjs
@@ -7,23 +7,25 @@ const token = process.env.DISCORD_BOT_TOKEN;
 const {Client, GatewayIntentBits} = discord;
 
 const intents = [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.MessageContent
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
 ];
 
 const client = new Client({intents});
 client.login(token);
 
-client.on('messageCreate', (message) => {
-  if (message.content.startsWith('MM:') && message.content.endsWith('?')) {
-    fetch('https://yesno.wtf/api')
-      .then((response) => response.json())
-      .then((response) => {
+client.on("messageCreate", (message) => {
+    if (message.content.startsWith("MM:") === false || message.content.endsWith("?") === false) {
+        return;
+    }
+
+    fetch("https://yesno.wtf/api")
+    .then((response) => response.json())
+    .then((response) => {
         message.reply({
-          content: response.answer,
-          files: [response.image]
+            content: response.answer,
+            files: [response.image]
         });
-      });
-  }
+    });
 });
